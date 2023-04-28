db4867f8d9b9847e11f9842e11856cf267df01cb
YEHUDI LARA <ylara@bmsoluciones.cl>
2023-04-28 16:35:43 -0400

    act

--- 03-app-gifts/src/app/gifs/gifs.module.ts
+++ 03-app-gifts/src/app/gifs/gifs.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    SearchBoxComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule,
